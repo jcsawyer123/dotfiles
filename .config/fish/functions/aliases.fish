b73ed4cf49f58255667985846cc8f457eef34aac
Joshua Sawyer <joshua@jcsawyer.me>
2020-05-12 00:08:11 +0100

    Created aliases for fish

---
+++ .config/fish/functions/aliases.fish
@@ -0,0 +1,16 @@
+# Navigation
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../../"
+
+# Shortcuts
+alias fishconfig="nano ~/dotfiles/.config/fish/config.fish"
+alias fishalias="nano ~/dotfiles/.config/fish/functions/aliases.fish"
+alias fishreload="source ~/.config/fish/config.fish"
+alias dotsync="~/dotfiles/bootstrap.sh"
+alias sn="shutdown now"
+alias pintel="sudo prime-select intel"
+alias pnvidia="sudo prime-select nvidia"
+alias pwhat="prime-select query"
+
